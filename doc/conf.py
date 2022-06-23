eafaa6c4eed1bf37d4a285ab722618e98e310652
Andrew Crabb <crabba@amazon.com>
2022-06-23 09:32:39 -0400

    sphinxdoc theme

--- doc/conf.py
+++ doc/conf.py
@@ -47,7 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'furo'
+html_theme = 'sphinxdoc'
 html_theme_path = ["."]
 
 # Add any paths that contain custom static files (such as style sheets) here,
